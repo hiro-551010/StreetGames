0b447a966e1bfee6380d34f027451e7a31fa08b1
hiro-551010 <hiro551010@gmail.com>
2021-12-22 11:38:47 +0900

    お問い合わせ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 @extends('users.header')
 
-{{$host_tournaments}}
-@section('header')
-自分がホストの大会
 
-@endforeach
+@section('header')
+
 @endsection
 
---
+++ resources/views/users/contact.blade.php
@@ -0,0 +1,4 @@
+@extends('users.header')
+@section('header')
+    <h1>お問い合わせページ</h1>
+@endsection
